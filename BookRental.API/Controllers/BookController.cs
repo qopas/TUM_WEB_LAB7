89333ed4a2eb2ffbaa28afb4230d0e874c41d62e
qopas <grigoras.dumiitru@gmail.com>
2025-05-15 12:29:18 +0300

    Create book controller with get all method

---
+++ BookRental.API/Controllers/BookController.cs
@@ -0,0 +1,23 @@
+﻿using BookRental.Domain.Entities;
+using BookRental.Domain.Interfaces.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookRental.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class BookController: ControllerBase
+{
+    private readonly IBookRepository _bookRepository;
+
+    public BookController(IBookRepository bookRepository)
+    {
+        _bookRepository = bookRepository;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Book>>> GetBooks()
+    {
+        var books = _bookRepository.GetAllAsync();
+        return Ok(books);
+    }
+}
