d456e0dbd6a3f9778ccf3fc10e5c1d0cca4c1414
qopas <grigoras.dumiitru@gmail.com>
2025-05-16 13:51:24 +0300

    Added customerController with CRUD operations

---
+++ BookRental.API/Controllers/CustomerController.cs
@@ -0,0 +1,86 @@
+﻿using Application.Mapping;
+using BookRental.Domain.DTOs.Customer;
+using BookRental.Domain.Entities;
+using BookRental.Domain.Interfaces.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookRental.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CustomerController : ControllerBase
+{
+    private readonly IRepository<Customer> _customerRepository;
+
+    public CustomerController(IRepository<Customer> customerRepository)
+    {
+        _customerRepository = customerRepository;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(List<CustomerDto>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> GetCustomers()
+    {
+        var customers = await _customerRepository.GetAllAsync();
+        return Ok(customers.ToDtoList());
+    }
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(typeof(CustomerDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> GetCustomer(string id)
+    {
+        var customer = await _customerRepository.GetByIdAsync(id);
+        if (customer == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(customer.ToDto());
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(CustomerDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> CreateCustomer([FromBody] CreateCustomerDto createCustomer)
+    {
+        var customer = createCustomer.ToEntity();
+        var createdCustomer = await _customerRepository.AddAsync(customer);
+        return Ok(createdCustomer.ToDto());
+    }
+
+    [HttpPut]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> UpdateCustomer([FromBody] UpdateCustomerDto updateCustomer)
+    {
+        var existingCustomer = await _customerRepository.GetByIdAsync(updateCustomer.Id);
+        if (existingCustomer == null)
+        {
+            return NotFound();
+        }
+
+        await _customerRepository.UpdateAsync(updateCustomer.ToEntity(existingCustomer));
+        return Ok($"Customer with id: {updateCustomer.Id} was successfully updated");
+    }
+
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> DeleteCustomer(string id)
+    {
+        var customer = await _customerRepository.GetByIdAsync(id);
+        if (customer == null)
+        {
+            return NotFound();
+        }
+
+        await _customerRepository.DeleteAsync(id);
+        return Ok($"Customer with id: {id} deleted");
+    }
+}
