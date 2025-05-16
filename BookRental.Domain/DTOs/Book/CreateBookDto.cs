c088bbf3ec6c3868123a18bf1daeaed5c0286d11
qopas <grigoras.dumiitru@gmail.com>
2025-05-16 11:33:28 +0300

    Added CreateBookDto.cs

---
+++ BookRental.Domain/DTOs/Book/CreateBookDto.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookRental.Domain.DTOs.Book;
+
+public class CreateBookDto
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public DateTime PublicationDate { get; set; }
+    public string GenreId { get; set; }
+    public int AvailableQuantity { get; set; } 
+    public decimal RentalPrice { get; set; }
+    
+    public Entities.Book ToEntity()
+    {
+        return new Entities.Book
+        {
+            Title = Title,
+            Author = Author,
+            PublicationDate = PublicationDate,
+            GenreId = GenreId,
+            AvailableQuantity = AvailableQuantity,
+            RentalPrice = RentalPrice
+        };
+    }
+}
