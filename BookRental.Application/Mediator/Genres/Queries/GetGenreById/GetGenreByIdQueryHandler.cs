94f4d2f851b2046ffbd327ca5a804d224f55035a
qopas <grigoras.dumiitru@gmail.com>
2025-05-16 16:59:51 +0300

    Addded mediatR, querry and handlers for GetGenre and GetGenreById

---
+++ BookRental.Application/Mediator/Genres/Queries/GetGenreById/GetGenreByIdQueryHandler.cs
@@ -0,0 +1,16 @@
+﻿using Application.Mapping;
+using BookRental.Domain.DTOs.Genre;
+using BookRental.Domain.Entities;
+using BookRental.Domain.Interfaces.Repositories;
+using MediatR;
+
+namespace Application.Mediator.Genres.Queries.GetGenreById;
+
+public class GetGenreByIdQueryHandler(IRepository<Genre> genreRepository) : IRequestHandler<GetGenreByIdQuery, GenreDto>
+{
+    public async Task<GenreDto> Handle(GetGenreByIdQuery request, CancellationToken cancellationToken)
+    {
+        var genre = await genreRepository.GetByIdAsync(request.Id);
+        return genre?.ToDto();
+    }
+}
---
+++ BookRental.Application/Mediator/Genres/Queries/GetGenres/GetGenresQueryHandler.cs
@@ -0,0 +1,16 @@
+﻿using Application.Mapping;
+using BookRental.Domain.DTOs.Genre;
+using BookRental.Domain.Entities;
+using BookRental.Domain.Interfaces.Repositories;
+using MediatR;
+
+namespace Application.Mediator.Genres.Queries.GetGenres;
+
+public class GetGenresQueryHandler(IRepository<Genre> genreRepository) : IRequestHandler<GetGenresQuery, IEnumerable<GenreDto>>
+{
+    public async Task<IEnumerable<GenreDto>> Handle(GetGenresQuery request, CancellationToken cancellationToken)
+    {
+        var genres = await genreRepository.GetAllAsync();
+        return genres.ToDtoList();
+    }
+}
