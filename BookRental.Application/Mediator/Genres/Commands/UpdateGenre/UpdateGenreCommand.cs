f90cc011f12daa1afb0cb99acc01757cfb799aaf
qopas <grigoras.dumiitru@gmail.com>
2025-05-16 17:07:04 +0300

    Added MediatR for updateGenreCommand

---
+++ BookRental.Application/Mediator/Genres/Commands/UpdateGenre/UpdateGenreCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Application.Mediator.Genres.Commands.UpdateGenre;
+
+public class UpdateGenreCommand : IRequest<bool>
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+}
