10f77d75c678d5b07acabdb1ffb9119b957a496d
Grigoras Dumitru <dgrigoras@sapec.md>
2025-05-19 14:28:26 +0300

    Added basic repositories for entities

---
+++ BookRental.Infrastructure/Repositories/CustomerRepository.cs
@@ -0,0 +1,12 @@
+﻿using BookRental.Domain.Entities;
+using BookRental.Domain.Interfaces.Repositories;
+using BookRental.Infrastructure.Data;
+
+namespace BookRental.Infrastructure.Repositories;
+
+public class CustomerRepository : Repository<Customer>, ICustomerRepository
+{
+    public CustomerRepository(BookRentalDbContext dbContext) : base(dbContext)
+    {
+    }
+}
---
+++ BookRental.Infrastructure/Repositories/GenreRepository.cs
@@ -0,0 +1,12 @@
+﻿using BookRental.Domain.Entities;
+using BookRental.Domain.Interfaces.Repositories;
+using BookRental.Infrastructure.Data;
+
+namespace BookRental.Infrastructure.Repositories;
+
+public class GenreRepository : Repository<Genre>, IGenreRepository
+{
+    public GenreRepository(BookRentalDbContext dbContext) : base(dbContext)
+    {
+    }
+}
