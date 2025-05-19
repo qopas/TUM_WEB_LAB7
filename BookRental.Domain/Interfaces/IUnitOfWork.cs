200f25583b8b62a106e2b9753258e83a8c39fd13
Grigoras Dumitru <dgrigoras@sapec.md>
2025-05-19 14:16:55 +0300

    Added basic unit of work

---
+++ BookRental.Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using BookRental.Domain.Interfaces.Repositories;
+
+namespace BookRental.Domain.Interfaces;
+
+public interface IUnitOfWork : IDisposable
+{
+    IRepository<T> Repository<T>() where T : class;
+    Task<int> SaveChangesAsync();
+}
---
+++ BookRental.Infrastructure/Data/UnitOfWork.cs
@@ -0,0 +1,43 @@
+﻿using BookRental.Domain.Interfaces;
+using BookRental.Domain.Interfaces.Repositories;
+using BookRental.Infrastructure.Repositories;
+
+namespace BookRental.Infrastructure.Data;
+
+public class UnitOfWork(BookRentalDbContext dbContext) : IUnitOfWork
+{
+    private readonly Dictionary<Type, object> _repositories = new();
+    private bool _disposed = false;
+
+    public IRepository<T> Repository<T>() where T : class
+    {
+        var type = typeof(T);
+
+        if (!_repositories.ContainsKey(type))
+        {
+            _repositories[type] = new Repository<T>(dbContext);
+        }
+
+        return (IRepository<T>)_repositories[type];
+    }
+
+    public async Task<int> SaveChangesAsync()
+    {
+        return await dbContext.SaveChangesAsync();
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!_disposed && disposing)
+        {
+            dbContext.Dispose();
+            _disposed = true;
+        }
+    }
+}
