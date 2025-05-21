5259ed83e9b328e315ea4f7c9fb6a9209ab35469
Grigoras Dumitru <dgrigoras@sapec.md>
2025-05-21 14:37:08 +0300

    Remove Book Extension add mapper in dto

--- BookRental.Application/DTOs/Book/CreateBookDto.cs
+++ BookRental.Application/DTOs/Book/CreateBookDto.cs
@@ -21,4 +21,5 @@ public class CreateBookDto
             RentalPrice = RentalPrice
         };
     }
+    
 }
--- BookRental.Application/DTOs/Book/UpdateBookDto.cs
+++ BookRental.Application/DTOs/Book/UpdateBookDto.cs
@@ -9,4 +9,26 @@ public class UpdateBookDto
     public string? GenreId { get; set; }
     public int? AvailableQuantity { get; set; }
     public decimal? RentalPrice { get; set; }
+    public static BookRental.Domain.Entities.Book ToEntity(UpdateBookDto dto, BookRental.Domain.Entities.Book existingBook)
+    {
+        if (dto.Title != null)
+            existingBook.Title = dto.Title;
+                
+        if (dto.Author != null)
+            existingBook.Author = dto.Author;
+                
+        if (dto.PublicationDate.HasValue)
+            existingBook.PublicationDate = dto.PublicationDate.Value;
+                
+        if (dto.GenreId != null)
+            existingBook.GenreId = dto.GenreId;
+                
+        if (dto.AvailableQuantity.HasValue)
+            existingBook.AvailableQuantity = dto.AvailableQuantity.Value;
+                
+        if (dto.RentalPrice.HasValue)
+            existingBook.RentalPrice = dto.RentalPrice.Value;
+                
+        return existingBook;
+    }
 }
