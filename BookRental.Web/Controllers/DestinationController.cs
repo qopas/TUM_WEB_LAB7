5684297e4644ee676f6e0ecff2cd030e4bf3d8a9
Grigoras Dumitru <dgrigoras@sapec.md>
2025-05-26 21:29:48 +0300

    Added Destination page

---
+++ BookRental.Web/Controllers/DestinationController.cs
@@ -0,0 +1,76 @@
+﻿using Application.Destination.Commands.CreateDestination;
+using Application.Destination.Commands.UpdateDestination;
+using Application.Destination.Commands.DeleteDestination;
+using Application.Destination.Queries.GetDestinationById;
+using Microsoft.AspNetCore.Mvc;
+using Application.Destination.Queries.GetDestinations;
+using BookRental.Web.Models;
+using MediatR;
+
+namespace BookRental.Web.Controllers;
+
+public class DestinationController(IMediator mediator) : BaseWebController
+{
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        return await ExecuteViewAsync(async () =>
+        {
+            var destinations = await mediator.Send(new GetDestinationsQuery());
+            return destinations.Select(DestinationViewModel.FromDto);
+        });
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetDestinationsData()
+    {
+        return await ExecuteAsync(async () =>
+        {
+            var destinations = await mediator.Send(new GetDestinationsQuery());
+            return new { 
+                data = destinations.Select(destination => new {
+                    id = destination.Id,
+                    name = destination.Name,
+                    address = destination.Address,
+                    city = destination.City,
+                    contactPerson = destination.ContactPerson,
+                    phoneNumber = destination.PhoneNumber
+                })
+            };
+        });
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Details(string id)
+    {
+        return await ExecuteAsync(async () =>
+        {
+            var destination = await mediator.Send(new GetDestinationByIdQuery { Id = id });
+            return DestinationViewModel.FromDto(destination);
+        });
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Create(DestinationViewModel model)
+    {
+        return await ExecuteAsync(async () =>
+            await mediator.Send(model.ToCreateCommand())
+        );
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Edit(DestinationViewModel model)
+    {
+        return await ExecuteAsync(async () =>
+            await mediator.Send(model.ToUpdateCommand())
+        );
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Delete(string id)
+    {
+        return await ExecuteAsync(async () =>
+            await mediator.Send(new DeleteDestinationCommand { Id = id })
+        );
+    }
+}
---
+++ BookRental.Web/Models/DestinationViewModel.cs
@@ -0,0 +1,53 @@
+﻿using Application.Destination.Commands.CreateDestination;
+using Application.Destination.Commands.UpdateDestination;
+using Application.DTOs.Destination;
+
+namespace BookRental.Web.Models;
+
+public class DestinationViewModel
+{
+    public string? Id { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+    public string ContactPerson { get; set; }
+    public string PhoneNumber { get; set; }
+    
+    public static DestinationViewModel FromDto(DestinationDto dto)
+    {
+        return new DestinationViewModel
+        {
+            Id = dto.Id,
+            Name = dto.Name,
+            Address = dto.Address,
+            City = dto.City,
+            ContactPerson = dto.ContactPerson,
+            PhoneNumber = dto.PhoneNumber
+        };
+    }
+
+    public CreateDestinationCommand ToCreateCommand()
+    {
+        return new CreateDestinationCommand
+        {
+            Name = Name,
+            Address = Address,
+            City = City,
+            ContactPerson = ContactPerson,
+            PhoneNumber = PhoneNumber
+        };
+    }
+    
+    public UpdateDestinationCommand ToUpdateCommand()
+    {
+        return new UpdateDestinationCommand
+        {
+            Id = Id,
+            Name = Name,
+            Address = Address,
+            City = City,
+            ContactPerson = ContactPerson,
+            PhoneNumber = PhoneNumber
+        };
+    }
+}
