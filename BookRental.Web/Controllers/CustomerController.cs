89a66edff1e85a6e1db472f38af0b7e6238e8cb2
Grigoras Dumitru <dgrigoras@sapec.md>
2025-05-27 10:30:05 +0300

    Added page for customer

---
+++ BookRental.Web/Controllers/CustomerController.cs
@@ -0,0 +1,67 @@
+﻿using Application.Customer.Commands.DeleteCustomer;
+using Application.Customer.Queries.GetCustomerById;
+using Microsoft.AspNetCore.Mvc;
+using Application.Customer.Queries.GetCustomers;
+using BookRental.Web.Models;
+using MediatR;
+
+namespace BookRental.Web.Controllers;
+
+public class CustomerController(IMediator mediator) : BaseWebController
+{
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        return await ExecuteViewAsync(async () =>
+        {
+            var customers = await mediator.Send(new GetCustomersQuery());
+            return customers.Select(CustomerViewModel.FromDto);
+        });
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetCustomersData()
+    {
+        return await ExecuteAsync(async () =>
+        {
+            var customers = await mediator.Send(new GetCustomersQuery());
+            return new { 
+                data = customers.Select(CustomerViewModel.FromDto)
+            };
+        });
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Details(string id)
+    {
+        return await ExecuteAsync(async () =>
+        {
+            var customer = await mediator.Send(new GetCustomerByIdQuery { Id = id });
+            return CustomerViewModel.FromDto(customer);
+        });
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Create(CustomerViewModel model)
+    {
+        return await ExecuteAsync(async () =>
+            await mediator.Send(model.ToCreateCommand())
+        );
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Edit(CustomerViewModel model)
+    {
+        return await ExecuteAsync(async () =>
+            await mediator.Send(model.ToUpdateCommand())
+        );
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Delete(string id)
+    {
+        return await ExecuteAsync(async () =>
+            await mediator.Send(new DeleteCustomerCommand { Id = id })
+        );
+    }
+}
---
+++ BookRental.Web/Models/CustomerViewModel.cs
@@ -0,0 +1,49 @@
+﻿using Application.Customer.Commands.CreateCustomer;
+using Application.Customer.Commands.UpdateCustomer;
+using Application.DTOs.Customer;
+
+namespace BookRental.Web.Models;
+
+public class CustomerViewModel
+{
+    public string? Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+    
+    public static CustomerViewModel FromDto(CustomerDto dto)
+    {
+        return new CustomerViewModel
+        {
+            Id = dto.Id,
+            FirstName = dto.FirstName,
+            LastName = dto.LastName,
+            Address = dto.Address,
+            City = dto.City
+        };
+    }
+
+    public CreateCustomerCommand ToCreateCommand()
+    {
+        return new CreateCustomerCommand
+        {
+            FirstName = FirstName,
+            LastName = LastName,
+            Address = Address,
+            City = City
+        };
+    }
+    
+    public UpdateCustomerCommand ToUpdateCommand()
+    {
+        return new UpdateCustomerCommand
+        {
+            Id = Id,
+            FirstName = FirstName,
+            LastName = LastName,
+            Address = Address,
+            City = City
+        };
+    }
+}
