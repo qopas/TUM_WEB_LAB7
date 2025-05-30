ecec1657e9a5e4f739ed6a79cd7d7e1c659694b1
Grigoras Dumitru <dgrigoras@sapec.md>
2025-05-30 10:48:00 +0300

    Migr: GrigorasDumitru_added_auditable_entities

---
+++ BookRental.Infrastructure/Migrations/20250530074526_GrigorasDumitru_added_auditable_entities.cs
@@ -0,0 +1,399 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookRental.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class GrigorasDumitru_added_auditable_entities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customers_AspNetUsers_ApplicationUserId",
+                table: "Customers");
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreatedAt",
+                table: "Rents",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Rents",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "UpdatedAt",
+                table: "Rents",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UpdatedBy",
+                table: "Rents",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreatedAt",
+                table: "RefreshTokens",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "RefreshTokens",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "DeletedAt",
+                table: "RefreshTokens",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DeletedBy",
+                table: "RefreshTokens",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "RefreshTokens",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "UpdatedAt",
+                table: "RefreshTokens",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UpdatedBy",
+                table: "RefreshTokens",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreatedAt",
+                table: "Genres",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Genres",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreatedAt",
+                table: "Destinations",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Destinations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "DeletedAt",
+                table: "Destinations",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DeletedBy",
+                table: "Destinations",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Destinations",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "UpdatedAt",
+                table: "Destinations",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UpdatedBy",
+                table: "Destinations",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreatedAt",
+                table: "Customers",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "DeletedAt",
+                table: "Customers",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DeletedBy",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Customers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "UpdatedAt",
+                table: "Customers",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UpdatedBy",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreatedAt",
+                table: "Books",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "DeletedAt",
+                table: "Books",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DeletedBy",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Books",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "UpdatedAt",
+                table: "Books",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UpdatedBy",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customers_AspNetUsers_ApplicationUserId",
+                table: "Customers",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customers_AspNetUsers_ApplicationUserId",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Rents");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Rents");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Rents");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "Rents");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedBy",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Genres");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Genres");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Destinations");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Destinations");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Destinations");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedBy",
+                table: "Destinations");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Destinations");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Destinations");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "Destinations");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedBy",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedBy",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "Books");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customers_AspNetUsers_ApplicationUserId",
+                table: "Customers",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
